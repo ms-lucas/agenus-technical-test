3da2d4707e8fd842a12847ae6dd77557e614ad81
Lucas Moreira <ms.lucasmoreira@gmail.com>
2025-10-07 12:31:16 -0300

    chore: configure zod type provider and openapi documentation

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
 import { fastify } from "fastify";
+import {
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { docsConfig } from "./plugins/docs-config";
 
-export const app = fastify();
+export const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(docsConfig);
