befb603c6b5a852feccf47de9796af552e008682
Lucas Moreira <ms.lucasmoreira@gmail.com>
2025-10-10 16:00:31 -0300

    chore: remove health check route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,6 @@ import { tasksRoutes } from "./tasks-routes";
 import { usersRoutes } from "./users-routes";
 
 export async function routes(app: FastifyInstance) {
-	app.get("/health", async () => ({ status: "ok" }));
 	app.register(usersRoutes, { prefix: "/users" });
 	app.register(tasksRoutes, { prefix: "/tasks" });
 }
